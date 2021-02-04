948bd42635e42757bcfd5e9139adb92e143c3755
hind <hindsamiri2@gmail.com>
2021-02-04 19:11:41 +0100

    llamar los métodos propios de cada clase

--- src/main/java/Prueba.java
+++ src/main/java/Prueba.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import t6AHind.FrontendPrg;
+import t6AHind.PrgBackend;
 import t6AHind.Programadore;
 import t6AHind.ProgramerWeb;
 
@@ -21,25 +22,37 @@ public class Prueba {
         
         //lista de programadores
         Programadore p1=new Programadore("juan","juan","AA5468L",1000);
-        Programadore p2=new Programadore("jack","jack","AF8898L",1500);
-        Programadore p3=new Programadore("maria","maria","FI8974M",1500);
+        Programadore p2=new ProgramerWeb(1800,"juan","juan","AA5468L",1000);
+        Programadore p3=new FrontendPrg(1600,1500,"zack","zack","ER5897L",1000);
+        Programadore p4=new PrgBackend(1500,"alice","alice","As5698kl",0,2000);
         
-        //lista de ProgaramadoresWeb
-        ProgramerWeb pw1=new ProgramerWeb(1800,"juan","juan","AA5468L",1000);
-        ProgramerWeb pw2=new ProgramerWeb(1900,"maria","maria","FI8974M",1500);
-        
-        //lista de frontend Programers
-        FrontendPrg f1=new FrontendPrg(0,1800,"juan","juan","AA5468L",1000);
-        FrontendPrg f2=new FrontendPrg(1600,1500,"zack","zack","ER5897L",1000);
-        
-        //lista de Backend Programers
-        FrontendPrg fend1=new FrontendPrg(1500,1500,"alice","alice","As5698kl",0);
-        FrontendPrg fend2=new FrontendPrg(1000,0,"juan","juan","AA5468L",1000);
+       
         
         //anadir Programadores a la lista de Programadores
-        programadores.add(new Programadore("juan","juan","AA5468L",1000));
-        programadores.add(new Programadore("maria","maria","FI8974M",1500));
-        
+        programadores.add(p1);
+        programadores.add(p2);
+        programadores.add(p3);
+        programadores.add(p4);
+      
+        //llamar los métodos propios de cada clase
+        System.out.println("-----------------------------");
+        for (Programadore aux : programadores) {
+            System.out.println(aux);   
+            aux.Programar();
+            // Conversiones explícitas
+            if (aux instanceof ProgramerWeb){
+                ((ProgramerWeb) aux).TestarPaginaWeb();
+            }
+            if (aux instanceof FrontendPrg){
+                FrontendPrg tmp = (FrontendPrg) aux;
+                tmp.PgoramarFrontend();;
+            }
+            if (aux instanceof PrgBackend){
+                PrgBackend tmp = (PrgBackend) aux;
+                tmp.PgoramarBackend();
+            }
+            
+        }
         
     }
 }
